d402ddc7ecfc27e9cdcdf63ead66aafdaa1cecd9
Aaron Quinlan <arq5x@qbm.local>
2010-12-14 00:10:38 -0500

    Added new gzip test file and updated test.py to illustrate overlap with gzipped BED file.

--- test.py
+++ test.py
@@ -29,6 +29,30 @@ def main():
     hits = rmskFile.findOverlaps("chr21", 9719768, 9739768, "-", 0.1)
     for hit in hits:
         print hit.chrom, hit.start, hit.end, hit.strand, hit.name, hit.other
+        
+    
+    rmskFile = IntervalFile("testData/rmsk.hg18.chr21.bed.2.gz")
+    rmskFile.loadIntoMap()
+
+    print "* gzip file, either strand, any overlap *"
+    hits = rmskFile.findOverlaps("chr21", 9719768, 9739768)
+    for hit in hits:
+        print hit.chrom, hit.start, hit.end, hit.strand, hit.name, hit.other
+
+    print "* gzip file, + strand, any overlap *"
+    hits = rmskFile.findOverlaps("chr21", 9719768, 9739768, "+")
+    for hit in hits:
+        print hit.chrom, hit.start, hit.end, hit.strand, hit.name, hit.other
+
+    print "* gzip file, either strand, any overlap *"
+    hits = rmskFile.findOverlaps("chr21", 9719768, 9739768, None, 0.1)
+    for hit in hits:
+        print hit.chrom, hit.start, hit.end, hit.strand, hit.name, hit.other
+
+    print "* gzip file, - strand, any overlap *"
+    hits = rmskFile.findOverlaps("chr21", 9719768, 9739768, "-", 0.1)
+    for hit in hits:
+        print hit.chrom, hit.start, hit.end, hit.strand, hit.name, hit.other
 
 if __name__ == "__main__":
     main()
