71d3a2431722e8c0668827f39e9693e3c14eed9f
Aaron Quinlan <aaronquinlan@gmail.com>
2011-03-07 11:03:11 -0500

    Updated test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 python setup.py build_ext -i
 PYTHONPATH=. python bedtools/tests/tests.py
-PYTHONPATH=. python ex1-intersect.py > /dev/null
+PYTHONPATH=. python examples/ex1-intersect.py > /dev/null
 nosetests --with-doctest --doctest-extension=.rst .
